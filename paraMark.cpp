c4102f6af7eda1a937ec05a505634fa88f011a49
Drew Ripberger <drew.ripberger@gmail.com>
2017-07-29 15:29:47 -0400

    Update paraMark.cpp

--- paraMark.cpp
+++ paraMark.cpp
@@ -35,7 +35,7 @@ int main(){
 
 	if(worldRank == 0){
 		int mainStart = time(NULL);
-		
+
 		//Master node listens for the other nodes to finish
 		while(finishedCount < worldSize){
 			MPI_Recv(&finishedSign, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -47,8 +47,8 @@ int main(){
 	}
 	else{
 		//Designates the portion the node needs to do and start that work
-		int rangeStart = std::round(worldRank-1*(mark/worldSize-1));
-		int rangeEnd = std::round(worldRank*(mark/worldSize-1));
+		int rangeStart = round(worldRank-1*(mark/worldSize-1));
+		int rangeEnd = round(worldRank*(mark/worldSize-1));
 		prime(rangeStart, rangeEnd);
 
 		//Signals the master node that the compute node has finished 
